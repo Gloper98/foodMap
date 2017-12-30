65ae32904e62ffc2ac43fe22f0a94caec781b20e
Gloper98 <littleannygutlop_2014@outlook.es>
2017-12-29 21:00:00 -0500

    Updating changes

--- js/principal-view.js
+++ js/principal-view.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	var $input_search_id = $('#input-search-id');
 	var $input_button = $('#input-button');
 	var $input_search = $('#input-search');
+	var $logo = $('#logo');
+	var $circle_button = $('#circle-button');
+	var $title = $('#title'); 
 	
 	function inputStyle() {
 		$input_button.addClass('inputChange');
@@ -12,6 +15,28 @@ $(document).ready(function(){
 		$input_search_id.removeClass('inputChange');
 	}
 	
+	function search(){
+		$circle_button.removeClass('show');
+		$circle_button.addClass('hide');
+		$title.removeClass('show'); 
+		$title.addClass('hide');
+		$logo.removeClass('negative-margin');
+		$input_search_id.addClass('show');
+		$input_search_id.removeClass('hide');
+	}
+	
+	function hideInput(){
+		$input_search_id.removeClass('show');
+		$input_search_id.addClass('hide');
+		$circle_button.removeClass('hide');
+		$circle_button.addClass('show');
+		$title.removeClass('hide'); 
+		$title.addClass('remove');
+		$logo.addClass('negative-margin');
+	}
+	
 	$input_search.focus(inputStyle);
 	$input_search.blur(inputStyleReverse);
+	$circle_button.click(search);
+	$logo.click(hideInput);
 })
