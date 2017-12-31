a91ddf2197c01e2b899ae581b2de0aaa5a8505d4
Gloper98 <littleannygutlop_2014@outlook.es>
2017-12-31 00:34:05 -0500

    Updating changes -input searcher-functionality

--- js/principal-view.js
+++ js/principal-view.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	var $logo = $('#logo');
 	var $circle_button = $('#circle-button');
 	var $title = $('#title'); 
+	var $search_result_div = $('#search-results');
 	
 	function inputStyle() {
 		$input_button.addClass('inputChange');
@@ -35,8 +36,50 @@ $(document).ready(function(){
 		$logo.addClass('negative-margin');
 	}
 	
+	function establishment_div_maker(){
+		for(var i=0; i<establishments_data.length; i++){
+			$search_result_div.append('<div class="col-xs-5 lot outline padding-right padding-left mg-col" data-type=' + establishments_data[i].type + ' data-district=' + establishments_data[i].district + '>' + 
+	  				'<div class="background-image">' + 
+	  				'<div class="dark-filter">' + 
+	  					'<h4 class="white restaurant-name">' + establishments_data[i].name + '</h4>' + 
+	  					'<p class="white slogan">' + establishments_data[i].slogan + '</p>' +
+							'<span class="icon-sad icons">' + '<p class="number">' + establishments_data[i].sad + '</p></span><span class="icon-smile icons"><p class="number">' + establishments_data[i].happy + '</p></span><span class="icon-wink icons"><p class="number">' + establishments_data[i].veryhappy + '</p></span><span class="icon-paperplane"></span>' +
+	  				'</div>' +
+	  				'</div>' +
+	  			'</div>');
+		}
+	}
+	
+	function random_search() { 
+      if ($(this).text().indexOf($input_search.val().toLowerCase()) !== -1) {
+        $(this).show();
+      }
+    }
+	
+	function type_search() { 
+      if ($(this).data('type').indexOf($input_search.val().toLowerCase()) !== -1) {
+        $(this).show();
+      }
+    }
+	
+	function district_search() { 
+      if ($(this).data('district').indexOf($input_search.val().toLowerCase()) !== -1) {
+        $(this).show();
+      }
+    }
+	
+	function general_filter() {
+	  var $lot = $('.lot');
+    $lot.hide();
+    $lot.each(random_search);
+    $lot.each(type_search);
+    $lot.each(district_search);
+  }
+
 	$input_search.focus(inputStyle);
 	$input_search.blur(inputStyleReverse);
 	$circle_button.click(search);
 	$logo.click(hideInput);
+	establishment_div_maker();
+	$input_search.keyup(general_filter);
 })
